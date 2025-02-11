28b919c64f35311bda72372ed60559f2ddeff767
hergar11 <hermebello11@gmail.com>
2025-02-11 17:22:55 +0100

    prueba

--- db.py
+++ db.py
@@ -2,3 +2,4 @@ import Structures
 
 tacan75x = Structures.Tacan(75.00, 'X', Structures.Coordinates(Structures.dms_to_dd(31,30,20), Structures.dms_to_dd(65,50,54))) # Khandahar Afganistan
 tacan78x = Structures.Tacan(78.00, 'X', Structures.Coordinates(Structures.dms_to_dd(31,49,45), Structures.dms_to_dd(64,13,1)))  # Camp Bastion Afganistan
+pruebha
