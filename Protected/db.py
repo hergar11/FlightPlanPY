c1acb1a7a442ec1451e10401e76721273e2518af
hergar11 <117866348+hergar11@users.noreply.github.com>
2025-04-29 16:23:08 +0200

    Alemania
    * prueba eliminada
    
    * Germani tacan

--- Protected/db.py
+++ Protected/db.py
@@ -8,3 +8,5 @@ class Syria:
     tacan21x = Structures.Tacan(21.00, 'X', Structures.Coordinates(Structures.dms_to_dd(37,00,56), Structures.dms_to_dd(35,26,53))) # Incirlick Turkey
     tacan107x = Structures.Tacan(107.00, 'X', Structures.Coordinates(Structures.dms_to_dd(34,34,35), Structures.dms_to_dd(32,57,46))) # Acrotiri Airport
     tacan84x = Structures.Tacan(84.00, 'X', Structures.Coordinates(Structures.dms_to_dd(32,39,58), Structures.dms_to_dd(35,11,2))) # Ramat David
+class ColdWarGermany:
+    tacan89x = Structures.Tacan(89.00, 'X', Structures.Coordinates(Structures.dms_to_dd(50, 3, 13), Structures.dms_to_dd(8, 38, 13))) # Ramstein AFB
